afe4d52203874064806991e1b91ac007e2af1e67
Abhiman67 <ironmanavengers6789@gmail.com>
2024-11-02 00:41:41 +0530

    .

---
+++ NOTES/heaps1.cpp
@@ -0,0 +1,46 @@
+/// heap is complete BT
+/// heap order property  max heap and min heap 
+#include<iostream>
+using namespace std;
+
+class Heap{
+public:
+    int arr[1000];
+    int size=0;
+    void insert(int val){
+        size=size+1;
+        int index=size;
+        arr[index]=val;
+
+        while(index>1){
+
+            int parent=index/2 ;
+            if(arr[index]>arr[parent]){
+                swap(arr[index],arr[parent]);
+
+            }
+            else return ;
+
+        }
+
+    }
+
+    void print (){
+         for(int i=1;i<=size;i++){
+            cout << arr[i] << " "; 
+         }
+    }
+};
+
+int main () {
+    
+    
+    Heap h;
+    h.insert(50);
+    h.insert(55);
+    h.insert(53);
+    h.insert(52);
+    h.insert(54);
+    h.print();
+
+}
