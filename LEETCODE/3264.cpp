63f607c890a7fe18f5845628feca30d8e5431a01
Abhiman67 <ironmanavengers6789@gmail.com>
2024-12-16 23:50:24 +0530

    .

---
+++ LEETCODE/3264.cpp
@@ -0,0 +1,25 @@
+/// 30 min laga diye iske ek sort karne ki wajah se you didnt have to sort it.
+/// use heap now 
+class Solution {
+public:
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        for (int iteration = 0; iteration < k; iteration++) {
+            int n = nums.size();
+            int min =0;
+
+        for (int i=0;i<n;i++){
+
+            if(nums[i]<nums[min]){
+                min=i;
+            }
+        }
+
+            if (!nums.empty()) {
+
+                nums[min] *= multiplier;
+            }
+        }
+ 
+        return nums;
+    }
+}; 
--- NOTES/STLS/stLuv.cpp
+++ NOTES/STLS/stLuv.cpp
@@ -1,2 +1,19 @@
 #include<iostream>
 using namespace std;
+
+
+int main (){
+
+// pair 2  data types
+ pair<int,string>  p;
+ // p me value lene ke lie 
+ p= make_pair(2,'abc');
+ // p= {2,"anc"}
+   
+ cout << p.first << " "  << p.second;
+
+/// v.size o(1)
+
+vector<int> v;
+
+}
