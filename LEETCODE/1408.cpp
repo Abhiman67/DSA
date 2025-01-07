405fa8215ee1e02f9664928614778c0b458e0bc3
Abhiman67 <ironmanavengers6789@gmail.com>
2025-01-07 21:29:41 +0530

    1408

---
+++ LEETCODE/1408.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<string> stringMatching(vector<string>& words) {
+        int n = words.size();
+
+        vector<string>store;
+
+        for(int i=0;i<n;i++){
+          
+          for(int j=0;j<n;j++){
+
+            if(i==j) continue;
+            if(words[j].find(words[i])!=string:: npos ){
+                store.push_back(words[i]);
+                break;
+            } 
+          }
+
+        }
+        return store;
+    }
+};
---
+++ NOTES/dp/basicsdp.cpp
@@ -0,0 +1,15 @@
+/// fibonacci 
+
+#include<iostream>
+using namespace std;
+int fibo(int n,vector<int>&dp){
+    if(n<=1) return 1;
+    
+}
+int main (){
+
+    int n;
+    cin n;
+    int dp[n+1];
+
+}
