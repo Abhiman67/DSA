a66475b65ab5c96df60ef67bb9a93987265a44d1
Abhiman67 <ironmanavengers6789@gmail.com>
2025-01-07 22:56:17 +0530

    dp

--- NOTES/dp/basicsdp.cpp
+++ NOTES/dp/basicsdp.cpp
@@ -4,12 +4,17 @@
 using namespace std;
 int fibo(int n,vector<int>&dp){
     if(n<=1) return 1;
-    
+    if(dp[n]!=-1) return dp[n];
+
+     return dp[n]=fibo(n-1,dp)+fibo(n-2,dp);
+
 }
 int main (){
 
     int n;
-    cin n;
-    int dp[n+1];
+   cin >> n;
+   vector<int>dp(n+1,-1);
 
+   cout << fibo(n,dp);
+   cout << "hello world";
 }
