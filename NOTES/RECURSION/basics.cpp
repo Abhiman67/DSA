88828d79d9ebeacc108cdddc2065baaafca16536
Abhiman67 <ironmanavengers6789@gmail.com>
2025-01-17 23:28:48 +0530

    .

---
+++ NOTES/RECURSION/basics.cpp
@@ -0,0 +1,44 @@
+//  #include<iostream>
+//  using namespace std;
+
+// int printf(int n){
+//     int cnt=0;
+
+//     if(n==0) return ;
+//     if(cnt==n) return;
+
+//     cout << cnt << endl;
+//      cnt++;
+//      printf();
+     
+// }
+
+//  int main(){
+
+//     int n=5 
+//     printf(n);
+//  }
+
+ /// 1-n using backtracking
+
+//  f(int i,int n){
+
+//    if(i>n) return;
+
+//    f(i+1,n)
+//    cout << i; 
+
+
+//  }
+
+
+//// fibonacci 
+
+ 
+
+#include<iostream>
+int main () {
+   std :: cout << "hello world ";
+   return 0;
+}
+ 
