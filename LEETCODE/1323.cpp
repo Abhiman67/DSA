ef278a5024f4cfc52985de9b85afaeaa46ba17c5
Abhiman67 <ironmanavengers6789@gmail.com>
2025-08-18 01:14:39 +0530

    1323

---
+++ LEETCODE/1323.cpp
@@ -0,0 +1,35 @@
+
+class Solution {
+public:
+    vector<int> arr(int num) {
+        vector<int> temp;
+        if(num == 0) return {0};
+        while(num > 0) {
+            temp.push_back(num % 10);
+            num = num / 10;
+        }
+        reverse(temp.begin(), temp.end()); 
+        return temp;
+    }
+
+    int back(vector<int>& store) {
+        int result = 0;
+        for(int i = 0; i < store.size(); i++) {
+            result = result * 10 + store[i];
+        }
+        return result;
+    }
+
+    int maximum69Number (int num) {
+        vector<int> store = arr(num);
+
+        for(int i = 0; i < store.size(); i++) {
+            if(store[i] == 6) {
+                store[i] = 9;
+                break;
+            }
+        }
+
+        return back(store);
+    }
+};
