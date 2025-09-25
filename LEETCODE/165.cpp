0ef6b8dbd4fb3b656b3b7383e99b9be5dcda240a
Abhiman67 <ironmanavengers6789@gmail.com>
2025-09-26 01:54:09 +0530

    LC POTD

--- LEETCODE/165.cpp
+++ LEETCODE/165.cpp
@@ -1,34 +1,34 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
-    vector<string> getTokens(string version) {
-        stringstream v(version);
-        string token_v = "";
-        vector<string> version_tokens;
-        while(getline(v, token_v, '.')) {
-            version_tokens.push_back(token_v);
-        }
-        return version_tokens;
-    }
-    int compareVersion(string version1, string version2) {
+    vector<string> Tokend(string version) {
+        vector<string> tokens;
+        stringstream ss(version);
+        string token;
 
-        vector<string> v1 = getTokens(version1);
-        vector<string> v2 = getTokens(version2);  
-        
-        int m = v1.size();
-        int n = v2.size();
-            
-        int  i = 0;
-        while(i < m || i < n) {
-            
-            int a = i < m ? stoi(v1[i]) : 0 ;
-            int b = i < n ? stoi(v2[i])  : 0 ;
-            
-            if(a > b)
-                return 1;
-            else if(b > a)
-                return -1;
-            else
-                i++;
+        while (getline(ss, token, '.')) {
+            tokens.push_back(token);
+        }
+        return tokens;
+    }
+
+    int compareVersion(string version1, string version2) {
+        vector<string> a = Tokend(version1);
+        vector<string> b = Tokend(version2);
+
+        int n = max(a.size(), b.size());
+
+        for (int i = 0; i < n; i++) {
+            int val1 = (i < a.size()) ? stoi(a[i]) : 0;
+            int val2 = (i < b.size()) ? stoi(b[i]) : 0;
+
+            if (val1 > val2) return 1;
+            if (val1 < val2) return -1;
         }
         return 0;
     }
