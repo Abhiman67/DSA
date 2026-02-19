883b3798f2748f4cfb41c40775d06d5bc70797c1
Abhiman67 <ironmanavengers6789@gmail.com>
2026-02-20 00:00:26 +0530

    cf potd

---
+++ CP31/800/8.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int t;
+    cin >> t;
+
+    while(t--){
+
+        int n,k;
+
+        cin >> n >> k;
+
+        int a[n];
+
+        bool found=false;
+
+        for(int i=0;i<n;i++){
+
+            cin >> a[i];
+        }
+
+
+        for(int i=0;i<n;i++){
+
+
+                if(a[i]==k){
+
+cout << "YES" << endl;
+                    found=true;
+                    break;
+                     
+                }
+        
+
+        }
+
+
+       if(!found) {
+
+            cout << "NO" << endl;
+
+        }
+
+       
+
+       
+        }
+
+         return 0; 
+    }
