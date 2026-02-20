9be3f7ab2210d75e983a23cd45fb9044484dc322
Abhiman67 <ironmanavengers6789@gmail.com>
2026-02-20 23:41:38 +0530

    refactor: improve variable declaration and formatting in main function

--- CP31/800/8.cpp
+++ CP31/800/8.cpp
@@ -8,45 +8,38 @@ int main(){
 
     while(t--){
 
-        int n,k;
+        long long n ,k;
 
         cin >> n >> k;
 
         int a[n];
-
-        bool found=false;
 
         for(int i=0;i<n;i++){
 
             cin >> a[i];
         }
 
+        bool found=false;
 
         for(int i=0;i<n;i++){
 
+            if(a[i]==k){
 
-                if(a[i]==k){
+                cout << "YES " << endl;
+                found=true;
+                break;
 
-cout << "YES" << endl;
-                    found=true;
-                    break;
-                     
-                }
-        
+            }
+
 
         }
 
-
-       if(!found) {
+        if(!found) {
 
             cout << "NO" << endl;
-
         }
 
-       
-
-       
-        }
-
-         return 0; 
     }
+
+    return 0;
+}
