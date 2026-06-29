7243560ccf2a1a25e062d451a72c3d1e96fca379
Abhiman67 <ironmanavengers6789@gmail.com>
2026-06-29 23:48:11 +0530

    lc potd

---
+++ LEETCODE/1967.cpp
@@ -0,0 +1,20 @@
+// brute force
+
+class Solution {
+public:
+    int numOfStrings(vector<string>& patterns, string word) {
+        
+        int cnt=0;
+
+        for(int i=0;i<patterns.size();i++){
+
+            if(word.find(patterns[i]) != std::string::npos){
+
+                 cnt++;
+            }
+
+        }
+
+        return cnt;
+    }
+};
